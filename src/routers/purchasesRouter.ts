7370594f41134e5b0ba47486c8937c8f662d1fee
Liaess <yann.melo@gmail.com>
2022-04-26 11:15:08 -0300

    Preparando revisão de código

--- src/routers/purchasesRouter.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import * as purchasesController from '../controllers/purchasesController.js'
-
-const purchasesRouter = Router()
-
-purchasesRouter.get('/purchases', purchasesController.purchases)
-
-export default purchasesRouter
--- src/routers/rechargeRouter.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import * as rechargeController from '../controllers/rechargeController.js'
-
-const rechargeRouter = Router()
-
-rechargeRouter.get('/recharge', rechargeController.rechargeCard)
-
-export default rechargeRouter
